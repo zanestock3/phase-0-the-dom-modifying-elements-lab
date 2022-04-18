fa9f32830d95c6731f067fb578100a816d60dd00
Zane Stock <zanestock3@gmail.com>
2022-04-18 16:05:38 -0500

    commit tree 93b63abc3ca032ea99a66b9ce0e93be64024896e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Zane is the champion"
+document.body.append(newHeader);
